793d62cc97b1b4c69b25d2512ac3aaa112bc2319
neilhickey93 <neilhickey93@gmail.com>
2013-12-10 16:32:27 +0000

    added listener and sender threads

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,7 +1,42 @@
 package Client;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+import Receiver.Receiver;
 
 public class Client {
 	
+	public Client() {
+		/* Create and start the threads */
+		Sender s = new Sender();
+		s.start();
+		Listener l = new Listener();
+		l.start();
+		
+	}
 	
+	private class Sender extends Thread {
+		Sender s = new Sender();
+		
+		@Override
+		public void run() {
+			s.sendPacket();
+		}
+	}
+	
+	
+	private class Listener extends Thread {
+		Receiver r = new Receiver();
+		
+		DatagramPacket p = new DatagramPacket(address);
+		@Override
+		public void run() {
+			
+			socket.receive(p);
+			r.receivePacket(packet);
+			
+		}
+	}
 
 }
