3e9790382570ee40a8cff68434f7d5a1436c1e6a
neilhickey93 <neilhickey93@gmail.com>
2013-12-10 16:54:02 +0000

    added a bit more

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,41 +1,73 @@
 package Client;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 
 import Receiver.Receiver;
+import Sender.Sender;
 
 public class Client {
 	
+	public static final String MCAST_ADDR = "230.0.0.1"; // hardcoded address for the multicast group
+	public static final int MCAST_PORT = 9013; // hardcoded port number for the multicast group
+	
+	private MulticastSocket socket;
+	private InetAddress address;
+	
 	public Client() {
-		/* Create and start the threads */
-		Sender s = new Sender();
-		s.start();
-		Listener l = new Listener();
-		l.start();
-		
+		try {
+			address = InetAddress.getByName(MCAST_ADDR);
+			socket = new MulticastSocket(MCAST_PORT);
+			socket.joinGroup(address);
+			
+			/* Create and start the threads */
+			Send t1 = new Send();
+			t1.start();
+			Listener t2 = new Listener();
+			t2.start();	
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
 	}
 	
-	private class Sender extends Thread {
+	/**
+	 * Sender Thread
+	 * Sends Packets on the socket, through the sender class
+	 *
+	 */
+	private class Send extends Thread {
 		Sender s = new Sender();
 		
 		@Override
 		public void run() {
-			s.sendPacket();
+			try {
+				if(s.hasPacketToSend())
+					socket.send(s.packetToSend());
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
-	
+	/**
+	 * Listener thread
+	 * Receives Packets from the nodes and forwards them to the receiver class
+	 *
+	 */
 	private class Listener extends Thread {
 		Receiver r = new Receiver();
 		
 		DatagramPacket p = new DatagramPacket(address);
 		@Override
 		public void run() {
-			
-			socket.receive(p);
-			r.receivePacket(packet);
-			
+			for(;;) {
+				socket.receive(p);
+				r.receivePacket(packet);
+			}
 		}
 	}
 
