386e1719f7f3ee78d5223153863bfe3fc7db592a
mashikag <littlesoldiergraba@gmail.com>
2013-12-13 23:48:29 +0000

    I have wrote a "quick" prototype of a client using multicast socket, to send a message one run client with a message in argument and to run another client that listens just simply don't type in command line argument when running the client. The code might not be so clear but I will make it easier to read and be more practical once I have time again.

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,32 @@
+package multicastclient;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+/**
+ *
+ * @author mashikag
+ */
 public class Client {
-
+    
+    public static void main(String[] args){
+        try{
+            MulticastSocket socket = new MulticastSocket(Constants.MCAST_PORT);
+            InetAddress mcastAddress = InetAddress.getByName(Constants.MCAST_ADDR);
+            socket.joinGroup(mcastAddress);
+            
+            if(args.length > 0){
+                //Sends the message that was provided in argument
+                System.out.println("Port number: "+socket.getLocalPort());
+                new Thread(new Sender(socket,args[0])).start();
+            }else{
+                //Acts as a receiver
+                new Thread(new Listener(socket)).start();
+            }
+                
+        }
+        catch(IOException e){e.printStackTrace();}
+        
+    }
 }
