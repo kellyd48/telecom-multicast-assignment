f35d9096cbd2e9a239b927f48eb4d9e6930d70ec
David Kelly <kellyd48@tcd.ie>
2013-12-15 22:23:16 +0000

    Removed the original java classes. (They're now in the mashikagClient branch)

--- src/Constants.java
+++
@@ -1,8 +0,0 @@
-package multicastclient;
-
-public class Constants {
-    public static final String MCAST_ADDR = "230.0.0.1"; // hardcoded address for the multicast group
-    public static final int MCAST_PORT = 9013; // hardcoded port number for the multicast group
-    
-    public static final int MAX_BUFFER = 1024; // maximum size for data in a packet  
-}
