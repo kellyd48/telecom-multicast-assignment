7c30178ac72b8bc2642ad1956620bf924a528a98
David Kelly <kellyd48@tcd.ie>
2013-12-15 16:27:17 +0000

    Fixed up comments in ClientNode ClientNodeList and Identifier classes.

--- src/Client/ClientNodeList.java
+++ src/Client/ClientNodeList.java
@@ -11,71 +11,85 @@ import java.util.Set;
  */
 public class ClientNodeList {
 	private HashMap<Integer, ClientNode> nodeList;
-	//Don't add this ID because it's the local client ID
+	// The local client ID
 	private Identifier myID;
-	
+
+	/**
+	 * ClientNodeList Constructor.
+	 * @param ID
+	 */
 	public ClientNodeList(Identifier ID){
 		nodeList = new HashMap<Integer, ClientNode>();
 		myID = new Identifier(ID);
 	}
-	
+
+	/**
+	 * Returns boolean for whether a ClientNode is contained in the list of nodes.
+	 * @param node
+	 * @return
+	 */
 	public boolean clientNodeLookup(ClientNode node){
-		/*
-		 * Returns boolean for whether a ClientNode is contained in the list of nodes.
-		 */
 		return nodeList.containsKey(node.getID().getIdentifier());
 	}
-	
+
+	/**
+	 * Adds a ClientNode to the list of nodes.
+	 * Does nothing if node already present.
+	 * @param node
+	 */
 	public void add(ClientNode node){
-		/*
-		 * Adds a ClientNode to the list of nodes.
-		 * Does nothing if node already present.
-		 */
 		if(!clientNodeLookup(node) && !myID.equals(node.getID())){
 			nodeList.put(node.getID().getIdentifier(), node);
 			System.out.println(node.toString());
 		}
 	}
-	
+
+	/**
+	 * Removes a ClientNode from a list of nodes.
+	 * Does nothing if node is not present.
+	 * @param node
+	 */
 	public void remove(ClientNode node){
-		/*
-		 * Removes a ClientNode from a list of nodes.
-		 * Does nothing if node is not present.
-		 */
 		if(clientNodeLookup(node))
 			nodeList.remove(node.getID().getIdentifier());
 	}
-	
+
+	/**
+	 * Returns the node at a certain index
+	 * @param index
+	 * @return
+	 */
 	public ClientNode get(int index){
-		/*
-		 * Returns the node at a certain index
-		 */
+		@SuppressWarnings("rawtypes")
 		Set nodes = nodeList.entrySet();
+		@SuppressWarnings("rawtypes")
 		Iterator iterator = nodes.iterator();
 		for(int i = 0; iterator.hasNext(); i++){
 			if(i == index){
+				@SuppressWarnings("unchecked")
 				Map.Entry<Integer, ClientNode> node = (Map.Entry<Integer, ClientNode>)iterator.next();
 				return (ClientNode)node.getValue();
 			}
 		}
 		return null;
 	}
-	
+
+	/**
+	 * Returns the size of the list
+	 * @return
+	 */
 	public int size(){
-		/*
-		 * Returns the size of the list
-		 */
 		return nodeList.size();
 	}
-	
+
 	public String toString(){
-		/*
-		 * Returns string representation of object.
-		 */
 		String toString = "";
+		@SuppressWarnings("rawtypes")
 		Set nodes = nodeList.entrySet();
+		@SuppressWarnings("rawtypes")
 		Iterator iterator = nodes.iterator();
 		while(iterator.hasNext()){
+			@SuppressWarnings("unchecked")
 			Map.Entry<Integer, ClientNode> node = (Map.Entry<Integer, ClientNode>)iterator.next();
 			toString += "Key: " + node.getKey() + " Client: " + node.getValue().toString() + "\n";
 		}
