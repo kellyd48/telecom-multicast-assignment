4753292fd0655205473ba2e242f77b79441de4d9
neilhickey93 <neilhickey93@gmail.com>
2013-12-23 20:52:13 +0000

    Simple image display

--- src/Receiver/Receiver.java
+++ src/Receiver/Receiver.java
@@ -1,15 +1,24 @@
 package Receiver;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import Client.*;
 
-public class Receiver {
+public class Receiver extends JPanel{
+	// eclipse added this
+	private static final long serialVersionUID = 1L;
+
 	public static enum RECEIVER_STATE {RECEIVING_IMAGE, FINISHED_RECEIVING, ERROR};
 
 	private ReceiverBuffer buffer;
 	private RECEIVER_STATE state;
 	private Ack ack;
 	private String outputFileName;
+	private Identifier id;
 
 	public Receiver(Identifier ID){
+		this.id = ID;
 		outputFileName = ID.toString()+".jpeg";
 	}
 
@@ -31,8 +40,14 @@ public class Receiver {
 					if(Ack.equals(Ack.nextExpectedAck(this.ack), new Ack(ack))){
 						this.ack.next();
 						buffer.run(Multicast.getData(packetReceived));
-						if(buffer.isComplete())
+						if(buffer.isComplete()) {
+							JFrame frame = new JFrame("Client ID: " + id.toString());
+							ImageDisplay panel = new ImageDisplay(outputFileName);
+							frame.getContentPane().add(panel);
+							frame.setSize(500, 500);
+							frame.setVisible(true);
 							state = RECEIVER_STATE.FINISHED_RECEIVING;
+						}
 					}
 					break;
 				default:
